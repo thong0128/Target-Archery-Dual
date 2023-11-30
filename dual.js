1ab7746e5b2243e9aab190b73b817199ee257e47
thong0128 <thong0128@gmail.com>
2023-11-30 08:22:07 +0700

    update

--- dual.js
+++ dual.js
@@ -20,3 +20,27 @@ function showAthleteList() {
     }
     document.getElementById("athletelist").innerHTML= content;
 }
+function selectAthleteList() {
+    let content=""
+    for (let i = 0; i < athleteList.length; i++) {
+        content+=`<option value="${athleteList[i].name}">${athleteList[i].name}</option>`;
+    }
+    document.getElementById("archer1").innerHTML= content;
+    document.getElementById("archer2").innerHTML= content;
+}
+let scoreSheet;
+function startMatch(athlete1,athlete2) {
+    athlete1 = athleteList.find(Archer => {
+        return Archer.name === document.getElementById("archer1").value});
+    athlete2 = athleteList.find(Archer => {
+        return Archer.name === document.getElementById("archer2").value});
+    // athlete1 = athleteList.find(({name}) => name === document.getElementById("archer1").value);
+    // athlete2 = athleteList.find(({name}) => name === document.getElementById("archer2").value);
+    if (athlete1 === athlete2)
+        alert("You can't beat your meat here :)");
+    else {
+        scoreSheet = new Scoresheet(athlete1, athlete2);
+        document.getElementById("a1name").innerText = scoreSheet.athlete1.name;
+        document.getElementById("a2name").innerText = scoreSheet.athlete2.name;
+    }
+}
