f8719fee1cde990ff762da4f6b7fde9c21d5c19d
thong0128 <thong0128@gmail.com>
2023-12-01 10:26:36 +0700

    update fix winrate calculator

--- athlete.js
+++ athlete.js
@@ -13,7 +13,7 @@ class Archer {
     // }
     calWinRate(){
         if (this.lose !==0)
-            this.winRate = this.win/(this.lose + this.win) * 100;
+            this.winRate =  (this.win/(this.lose + this.win) * 100).toFixed(2);
         else if (this.win > 0)
             this.winRate = 100;
         else
